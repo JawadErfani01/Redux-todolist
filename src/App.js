e181aaede8f7e469ab2aca10456ae79d41915391
awad erfani <jawad.erfani147@gmail.com>
2022-01-17 06:56:22 +0430

    React TailwindV3 Setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <main className="flex flex-col font-serif justify-center items-center text-white  h-screen bg-slate-800">
+      <h1 className="text-4xl hover:text-blue-600">Frontend With jawad</h1>
+      <p className="p-4 m-2 text-xl hover:bg-red-400 hover:rounded-xl">
+        Tailwind V3
+      </p>
+    </main>
+  );
+}
+
+export default App;
