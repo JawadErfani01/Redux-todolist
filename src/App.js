a1a9ff38fef1d0b08ea5547e35978e15d5aa2b17
Frontend <jawad.erfani147@gmail.com>
2022-02-02 15:30:11 +0430

    redux tutorile

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ function App() {
   return (
     <main className="flex flex-col font-serif justify-center items-center text-white  h-screen bg-slate-800">
       <h1 className="text-4xl hover:text-blue-600">Frontend With jawad</h1>
-      <p className="p-4 m-2 text-xl hover:bg-red-400 hover:rounded-xl">
+      <p className="p-4 m-2 text-xl hover:bg-gray-400 hover:rounded-full">
         Tailwind V3
       </p>
     </main>
