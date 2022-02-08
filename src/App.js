82442c5779217787062cb9bae135658c07116877
Frontend <jawad.erfani147@gmail.com>
2022-02-08 18:24:13 +0430

    redux installed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
+
+
 function App() {
   return (
     <main className="flex flex-col font-serif justify-center items-center text-white  h-screen bg-slate-800">
-      <h1 className="text-4xl hover:text-blue-600">Frontend With jawad</h1>
-      <p className="p-4 m-2 text-xl hover:bg-gray-400 hover:rounded-full">
-        Tailwind V3
-      </p>
+  <h1 className="text-4xl capitalize ">Redux tutorile</h1>
     </main>
   );
 }
