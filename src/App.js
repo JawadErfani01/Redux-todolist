9b5f78f6ee7013ff195b8ebdb84c1939ef202277
Jawad Erfani <jawad.erfani147@gmail.com>
2022-03-03 00:18:25 +0430

    it is samethings

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { useSelector, useDispatch } from "react-redux";
 import {
   increment,
@@ -56,6 +57,19 @@ function App() {
         ? "You sign in to my page now you can see everytings"
         : "To see samtings you have to login first"}
     </main>
+=======
+
+import Counter from './Components/Counter';
+import Login from './Components/Login';
+function App() {
+
+  return (
+    <div className="text-center p-6 text-white  h-screen bg-slate-800">
+      <h1 className="text-4xl capitalize ">Redux tutorile</h1>
+      <Counter/>
+      <Login/>
+    </div>
+>>>>>>> parent of cf73be0...  i get data from form with redux toolkit
   );
 }
 
