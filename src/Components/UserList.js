390a8bf0a400f46fedd191192d712a9f3220d40d
Jawad Erfani <jawad.erfani147@gmail.com>
2022-04-10 03:16:47 +0430

    removeHandaler has been changed

--- src/Components/UserList.js
+++ src/Components/UserList.js
@@ -1,18 +1,23 @@
 import React from "react";
+import { useDispatch} from "react-redux";
+import {
 
-function UserList({ data, handelUpdate,handelRemove }) {
+  removePersnoe,
 
+} from "../Features/todoRedux/todoReducers";
+function UserList({ data, handelUpdate }) {
+const dispath =useDispatch();
   return (
     <div>
       {data.length===0?<h1 className="text-2xl italic capitalize text-red-600">Your list is empaty</h1>:data.map((persone, index) => {
         return (
           <div className="flex justify-center " key={index}>
-            <div className=" flex justify-between items-center text-black  capitalize  border w-2/3 px-10 p-2 rounded-lg  m-4 mb-1 bg-cyan-500">
+            <div className=" flex justify-between items-center text-black  capitalize  border w-full md:w-2/3 px-10 p-2 rounded-lg  m-4 mb-1 bg-cyan-500">
               <h1>{persone.name}</h1>
               <h3 className="font-bold ">{persone.lastname}</h3>
               <button
                 className="bg-red-400 px-3 text-sm py-2 rounded-lg hover:scale-95 duration-300 hover:bg-red-500 shadow-lg cursor-pointer"
-                onClick={()=>handelRemove(persone.id)}
+                onClick={()=>dispath(removePersnoe(persone.id))}
               >
                 Remove
               </button>
